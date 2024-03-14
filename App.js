a63c0b7eb3e119a87ef60af0fc2b10e90b66227b
Srujan <srujan.is21@bmsce.ac.in>
2024-03-14 15:08:07 +0530

    Initalizating the project
    Added few dependencies
    - rn/material-top-tabs
    - rn/native
    - rn/stack
    - expo-camera
    - expo-sqlite
    - expo-status-bar
    - rn-gesture-handler
    - rn-pager-view
    - rn-qrcode-svg
    - rn-safe-area-context
    - rn-screens
    - rn-svg
    - rn-native-tab-view

--- App.js
+++ App.js
@@ -1,20 +1,45 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
+import { StyleSheet } from "react-native";
+import QRCodeGenerator from "./Screens/qrcode";
+import { createMaterialTopTabNavigator } from "@react-navigation/material-top-tabs";
+
+const Stack = createStackNavigator();
+const Tabs = createMaterialTopTabNavigator();
+
+function PayFlow() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Scan" component={Scan} />
+      <Stack.Screen name="Pay" component={Pay} />
+    </Stack.Navigator>
+  );
+}
+
+function ScanFlow() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="QR Code" component={QRCodeGenerator} />
+    </Stack.Navigator>
+  );
+}
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Tabs.Navigator tabBarPosition="bottom" initialRouteName="Payment">
+        {/* <Tabs.Screen name="QR" component={ScanFlow} /> */}
+        {/* <Tabs.Screen name="Payment" component={PayFlow} /> */}
+      </Tabs.Navigator>
+    </NavigationContainer>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
