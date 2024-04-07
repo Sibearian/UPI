3adff88ebfdc129ddc27819acb7f649c1b7eb027
Srujan <srujan.is21@bmsce.ac.in>
2024-04-07 13:16:51 +0530

    asdf

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,18 @@
 import { defineConfig } from "vite";
+import { resolve } from 'path'
 import mkcert from "vite-plugin-mkcert";
 
 export default defineConfig({
   server: { https: true, host: "0.0.0.0" }, // Not needed for Vite 5+
   plugins: [mkcert()],
   root: "src/",
-  build: { outDir: "../dist" },
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "src/index.html"),
+        pay: resolve(__dirname, "src/pay.html")
+      },
+    },
+    outDir: "../dist"
+  }
 });
